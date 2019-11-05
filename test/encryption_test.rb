c1c038ac1bb696fe77ba24a693894adc7c78b895
Linda Le <yueling1222@gmail.com>
2019-11-05 13:41:20 -0700

    Add method and test for spliting up message into individual letters and getting indexes

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -1,7 +1,18 @@
 require_relative 'test_helper'
+require './lib/alphabet'
 require './lib/key_generator'
 require './lib/encryption'
 
 class EncryptionTest < Minitest::Test
 
-end 
+  def test_it_exists
+    encryption = Encryption.new
+    assert_instance_of Encryption, encryption
+  end
+
+  def test_it_can_split_message_into_indexes
+    encryption = Encryption.new
+    assert_equal [7, 4, 11, 11, 14], encryption.get_message_indexes("hello")
+  end
+
+end
