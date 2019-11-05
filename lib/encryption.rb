17cd0790e36933ab7e873d3ca0bacc2386b9ee3a
Linda Le <yueling1222@gmail.com>
2019-11-05 14:52:35 -0700

    Add new method that outputs message with shifts applied and passing test

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,10 +1,9 @@
-require './lib/alphabet'
-
 class Encryption
   attr_reader :alphabet
 
   def initialize
     @alphabet = ("a".."z").to_a << " "
+    @shifts = []
   end
 
   def find_letter_starting_point(letter)
@@ -17,4 +16,39 @@ class Encryption
       find_letter_starting_point(letter)
     end
   end
+
+  def generate_random_key
+    key = rand(99999).to_s
+    key.rjust(5, "0")
+    key.chars.each_cons(2).map(&:join)
+  end
+
+  def calculate_offset_from_date
+    date = date.to_i
+    squared_total = (date ** 2)
+    squared_total.to_s[-4..-1]
+  end
+
+  def set_shifts(generate_random_key, calculate_offset_from_date)
+    @shifts << generate_random_key[0].to_i + calculate_offset_from_date[0].to_i
+    @shifts << generate_random_key[1].to_i + calculate_offset_from_date[1].to_i
+    @shifts << generate_random_key[2].to_i + calculate_offset_from_date[2].to_i
+    @shifts << generate_random_key[3].to_i + calculate_offset_from_date[3].to_i
+  end
+
+  def update_letter_indexes(message, set_shifts)
+    encrypted_message = get_message_indexes(message)
+    new_message = []
+    encrypted_message.map do |letter_index|
+      new_message << (letter_index + set_shifts.first)%27
+         set_shifts.rotate!
+    end
+    new_message
+  end
+
+  def encrypt
+    update_letter_indexes.map do char
+
+  end
+
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -15,4 +15,15 @@ class EncryptionTest < Minitest::Test
     assert_equal [7, 4, 11, 11, 14], encryption.get_message_indexes("hello")
   end
 
+  def test_it_shifts
+    encryption = Encryption.new
+    assert_equal [12, 14, 16, 18], encryption.set_shifts(["10", "11", "12", "13"], ["2", "3", "4", "5"])
+  end
+
+
+  def test_it_can_calculate_new_indexes
+    encryption = Encryption.new
+    assert_equal [8, 6, 14, 15, 15], encryption.update_letter_indexes("hello", [1, 2, 3, 4])
+  end
+
 end
