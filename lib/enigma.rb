a5db6551d581e53b1835b6205fcc9e35626b257a
Linda Le <yueling1222@gmail.com>
2019-11-05 19:07:05 -0700

    Add shell methods with encryption and decrpyt instance

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,11 +5,11 @@ require './lib/encryption'
 class Enigma
 
   def encrypt(message, key = nil, date = nil)
-
+    Encryption.new
   end
 
   def decrypt(message, key = nil, date= nil)
-
+    Decrypt.new
   end
 
 end
